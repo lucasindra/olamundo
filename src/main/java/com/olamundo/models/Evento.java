3256988a364e1c0d021363731a1e84a8af9fc00a
lucasindra <lpamplona@indracompany.com>
2018-09-17 09:59:11 -0300

    Projeto ola mundo criado

---
+++ src/main/java/com/olamundo/models/Evento.java
@@ -0,0 +1,32 @@
+package com.olamundo.models;
+
+public class Evento {
+	private double distancia;
+	private double km;
+	private double preco;
+	private double total;
+	
+	public double getDistancia() {
+		return distancia;
+	}
+	public void setDistancia(double distancia) {
+		this.distancia = distancia;
+	}
+	public double getKm() {
+		return km;
+	}
+	public void setKm(double km) {
+		this.km = km;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public double getTotal() {
+		return total;
+	}
+	
+	
+}
